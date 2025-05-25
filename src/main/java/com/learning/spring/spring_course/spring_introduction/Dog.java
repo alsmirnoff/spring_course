d5910bbb4bbafdcfe44363415b1c0b155b41a121
alsmirnoff <alex-sm-91@mail.ru>
2025-05-25 14:24:50 +0300

    Autowired, Qualifier, Value, Scope

--- src/main/java/com/learning/spring/spring_course/spring_introduction/Dog.java
+++ src/main/java/com/learning/spring/spring_course/spring_introduction/Dog.java
@@ -1,5 +1,12 @@
 package com.learning.spring.spring_course.spring_introduction;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dog implements Pet {
     // private String name;
     
@@ -12,10 +19,12 @@ public class Dog implements Pet {
         System.out.println("Bow-Wow");
     }
 
+    @PostConstruct
     protected void init(){
         System.out.println("Class Dog: init method");
     }
 
+    @PreDestroy
     private void destroy(){
         System.out.println("Class Dog: destroy method");
     }
--- src/main/java/com/learning/spring/spring_course/spring_introduction/Person.java
+++ src/main/java/com/learning/spring/spring_course/spring_introduction/Person.java
@@ -1,21 +1,34 @@
 package com.learning.spring.spring_course.spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
+    // @Autowired
+    // @Qualifier("catBean")
     private Pet pet;
+    // @Value("${person.surname}")
     private String surname;
+    // @Value("${person.age}")
     private int age;
 
-    // public Person(Pet pet) {
-    //     System.out.println("Person bean is created");
-    //     this.pet = pet;
-    // }
-
-    public Person(){
+    @Autowired
+    public Person(@Qualifier("catBean") Pet pet) {
         System.out.println("Person bean is created");
+        this.pet = pet;
     }
 
+    // public Person(){
+    //     System.out.println("Person bean is created");
+    // }
+
+    // @Autowired
+    // @Qualifier("dog")
     public void setPet(Pet pet) {
-        System.out.println("Class Person: set pet");
+        System.out.println("Class Person: set Pet");
         this.pet = pet;
     }
 
