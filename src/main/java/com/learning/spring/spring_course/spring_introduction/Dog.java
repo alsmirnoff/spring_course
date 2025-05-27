d3d72e95198af58504ef2e298b1a7c4892ee81b6
alsmirnoff <alex-sm-91@mail.ru>
2025-05-27 11:15:48 +0300

    switch javax api to jakarta api

--- src/main/java/com/learning/spring/spring_course/spring_introduction/Dog.java
+++ src/main/java/com/learning/spring/spring_course/spring_introduction/Dog.java
@@ -1,7 +1,7 @@
 package com.learning.spring.spring_course.spring_introduction;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
