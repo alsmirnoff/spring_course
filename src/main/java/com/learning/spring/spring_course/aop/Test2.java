20a3b16fdca32e3df854954be5b7629f1e05fd59
alsmirnoff <alex-sm-91@mail.ru>
2025-06-01 19:25:33 +0300

    lesson 2 afterreturning

---
+++ src/main/java/com/learning/spring/spring_course/aop/Test2.java
@@ -0,0 +1,18 @@
+package com.learning.spring.spring_course.aop;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test2 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        University university = context.getBean("university", University.class);
+        university.addStudents();
+        List<Student> students = university.getStudents();
+        System.out.println(students);
+
+        context.close();
+}
+}
---
+++ src/main/java/com/learning/spring/spring_course/aop/aspects/UniversityLoggingAspect.java
@@ -0,0 +1,36 @@
+package com.learning.spring.spring_course.aop.aspects;
+
+import java.util.List;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import com.learning.spring.spring_course.aop.Student;
+
+@Component
+@Aspect
+public class UniversityLoggingAspect {
+
+    @Before("execution(* getStudents())")
+    public void beforeGetStudentsLoggingAdvice(){
+        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение списка студентов перед методом getStudents");
+    }
+
+    @AfterReturning(pointcut = "execution(* getStudents())",
+                returning = "students")
+    public void afterReturningGetStudentsLoggingAdvice(List<Student> students){
+        Student firtsStudent = students.get(0);
+
+        String nameSurname = firtsStudent.getNameSurname();
+        nameSurname = "Mr. " + nameSurname;
+        firtsStudent.setNameSurname(nameSurname);
+
+        Double avgGrade = firtsStudent.getAvgGrade();
+        avgGrade = avgGrade + 1;
+        firtsStudent.setAvgGrade(avgGrade);
+
+        System.out.println("afterReturningGetStudentsLoggingAdvice: логируем получение списка студентов после выполнения метода getStudents");
+    }
+}
